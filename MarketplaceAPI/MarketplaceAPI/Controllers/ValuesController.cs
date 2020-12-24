9c04ffb964200991bdff49fdb2e1cf1009cf7616
monsior <monsiorpawel@gmail.com>
2020-12-24 14:04:00 +0100

    Add value controller for testing purposes

---
+++ MarketplaceAPI/MarketplaceAPI/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MarketplaceAPI.Data;
+using MarketplaceAPI.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace MarketplaceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors]
+    public class ValuesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ValuesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
+
+            return Ok(value);
+        }
+    }
+}
