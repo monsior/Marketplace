bea3e95cd8c2e73b26fabc016734d0aba2311557
monsior <monsiorpawel@gmail.com>
2021-01-20 11:38:05 +0100

    Add css styling for categories view

--- MarketplaceAPI/MarketplaceAPI/Models/Category.cs
+++ MarketplaceAPI/MarketplaceAPI/Models/Category.cs
@@ -9,6 +9,7 @@ namespace MarketplaceAPI.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ImageUrl { get; set; }
         public virtual ICollection<Auction> Auctions { get; set; }
     }
 }
